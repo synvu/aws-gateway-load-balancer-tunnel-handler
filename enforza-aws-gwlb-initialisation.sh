6da27c5d528c57bb895ef337fddf6fbc229f7216
Neil Briscoe <neil.briscoe@synvu.com>
2025-01-28 21:00:10 +0000

    Update enforza-aws-gwlb-initialisation.sh

--- enforza-aws-gwlb-initialisation.sh
+++ enforza-aws-gwlb-initialisation.sh
@@ -6,3 +6,6 @@ echo Mode is $1, In Int is $2, Out Int is $3, ENI is $4
 ip route add 10.0.0.0/8 dev $3
 ip route add 192.168.0.0/16 dev $3
 ip route add 172.16.0.0/12 dev $3
+
+echo 0 > /proc/sys/net/ipv4/conf/all/rp_filter
+echo 0 > /proc/sys/net/ipv4/conf/$2/rp_filter

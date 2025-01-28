fc17ca849134e3e03df0e8b1aaf663a97da458ac
Neil Briscoe <neil.briscoe@synvu.com>
2025-01-28 20:29:12 +0000

    Create enforza-aws-gwlb-initialisation.sh

---
+++ enforza-aws-gwlb-initialisation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "==> Setting up enforza AWS GWLB handler"
+echo Mode is $1, In Int is $2, Out Int is $3, ENI is $4
+
+ip route add 10.0.0.0/8 dev $3
+ip route add 192.168.0.0/16 dev $3
+ip route add 172.16.0.0/12 dev $3
